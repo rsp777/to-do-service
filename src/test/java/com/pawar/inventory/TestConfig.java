5eb3948a6bb6e9b728c5899d535ec69765a2f6b8
Ravindra Singh Pawar <raspawar007@outlook.com>
2024-07-03 00:54:07 +0530

    Updated code for junit

--- src/test/java/com/pawar/inventory/TestConfig.java
+++ src/test/java/com/pawar/inventory/TestConfig.java
@@ -1,8 +1,9 @@
 package com.pawar.inventory;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootConfiguration
 public class TestConfig {
     // Your test configuration here
 }
